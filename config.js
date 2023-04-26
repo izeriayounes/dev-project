d3089aab973bd58a5b3580238cc6169e794dc3cf
izeriayounes <izeria.younes@ine.inpt.ma>
2023-04-26 19:58:46 +0100

    initial commit

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { collection, getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAYKjV4qU4NSVKs2D2GmiIf61GsAgZokBM",
-    authDomain: "proj-dev-cbe36.firebaseapp.com",  
-    projectId: "proj-dev-cbe36",  
-    storageBucket: "proj-dev-cbe36.appspot.com",
-    messagingSenderId: "534732493500",  
-    appId: "1:534732493500:web:ab7b639f42c8c16c42b6b9"
-};
+    apiKey: "AIzaSyAzvVgvpFL540CyMGFJ8fr_7hta4d9BLkQ",
+    authDomain: "rpi-firestore-c73f2.firebaseapp.com",
+    projectId: "rpi-firestore-c73f2",
+    storageBucket: "rpi-firestore-c73f2.appspot.com",
+    messagingSenderId: "1006044062665",
+    appId: "1:1006044062665:web:1ff53dc012b2e72bfd3e18"
+  };
   
   
   // Initialize Firebase
